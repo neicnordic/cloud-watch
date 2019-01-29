44361b942571c8e3e4500d0c4383519a59639e60
Kim Brugger <kim.brugger@uib.no>
2019-01-29 16:28:51 +0100

    placeholders for the cli

---
+++ cli/cloud_watch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 
+ 
+ 
+ 
+ Kim Brugger (29 Jan 2019), contact: kim@brugger.dk
+"""
+
+import sys
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+import argparse
+import requests
