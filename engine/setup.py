ba730b9e46cfe87d1859670faae641806fa5f678
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-01-29 16:40:46 +0000

    first test version of the engine api

---
+++ engine/setup.py
@@ -0,0 +1,21 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Cloud-watch Engine',
+    'download_url': 'https://github.com/neicnordic',
+    'version': '1.0',
+    'install_requires': [
+        'flask',
+        'configparser',
+        'connexion',
+    ],
+    'packages': ['api'],
+    'scripts': [],
+    'name': 'cloud-watch-engine'
+}
+
+setup(**config)
