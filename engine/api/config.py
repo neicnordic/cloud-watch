8e7303b3f0841e86cdd112084157979e9355d483
Oliver Stein <oliver-dot-stein@hotmail.com>
2019-01-30 09:35:03 +0100

    created config class

---
+++ engine/api/config.py
@@ -0,0 +1,17 @@
+class CloudConfig:
+
+    class Provider:
+        def __init__(self, provider):
+            self
+
+    def __init__(self):
+
+
+        # load config yaml
+        config = {}
+        # populate all fields
+
+        self.providers = [self.Provider(provider) for provider in config]
+
+
+        pass
